44290d2d2e65fe670a0df5e82440aad5fcbdcab9
Lucas Fayolle <lfayolle1@gmail.com>
2016-01-23 14:40:12 +0100

    init irradie project with kirby and gulp-sass-php env.

--- gulpfile.js
+++ gulpfile.js
@@ -68,7 +68,7 @@ gulp.task('clean', require('del').bind(null, ['.tmp', 'dist']));
 // Watch Files For Changes & Reload
 gulp.task('serve', ['styles'], function () {
   browserSync.init({
-    proxy: 'quentin.dev'
+    proxy: 'irradie.dev'
   });
   // browserSync({
   //   notify: false,
