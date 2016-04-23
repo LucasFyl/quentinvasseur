7f7c3f8e5dca53cd0e82d393043d894f2cd948f6
Lucas Fayolle <lfayolle1@gmail.com>
2016-04-23 14:42:34 +0200

    home page

--- app/site/snippets/header.php
+++ app/site/snippets/header.php
@@ -3,7 +3,7 @@
 </header>
 
 <header class="main-header <?php echo $page->id() ?>">
-  <h1 class="logo">Quentin Vasseur</h1>
+  <h1 class="logo"><a href="<?php echo $site->url() ?>">Quentin Vasseur</a></h1>
   <nav>
     <?php foreach($pages->visible() as $p): ?>
       <a <?php e($p->isOpen(), ' class="active"') ?>  href="<?php echo $p->url() ?>"><?php echo $p->title() ?></a>
--- app/site/templates/default.php
+++ app/site/templates/default.php
@@ -1,4 +1,9 @@
 <?php snippet('head') ?>
 <?php snippet('header') ?>
 
+<main class="<?php echo $page->id() ?>">
+	<h1><?php echo $page->title() ?></h1>
+	<h3><?php echo $page->text() ?></h3>
+</main>
+
 <?php snippet('footer') ?>
