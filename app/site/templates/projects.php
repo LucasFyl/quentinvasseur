cf0957ff1ad2de654eb0946a2bd0166b8c2aca59
Lucas Fayolle <lfayolle1@gmail.com>
2016-04-24 17:49:02 +0200

    base template projects done

--- app/site/templates/projects.php
+++ app/site/templates/projects.php
@@ -2,12 +2,26 @@
 
 <?php snippet('header') ?>
 
-  <main role="main" class="main <?php echo $page->id() ?>">
+	<main role="main" class="main projects <?php echo $page->id() ?>">
+	
+		<aside class="projects-nav">
+			<ul>
+				<?php foreach($page->children()->visible() as $subpage): ?>
+				<li>
+					<h3><?php echo $subpage->title() ?></h3>
+				</li>
+				<?php endforeach; ?>
+			</ul>
+		</aside>
+		
+		<div class="projects-gallery">
+			<ul>
+				<?php foreach($page->children()->visible() as $subpage): ?>
+				<li>image</li>
+				<?php endforeach; ?>
+			</ul>
+		</div>			
 
-
-  <h1>projects.php</h1>
-
-  
-  </main>
+	</main>
 
 <?php snippet('footer') ?>
