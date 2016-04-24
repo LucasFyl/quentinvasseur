436fe6897342d87c2641da64be8f59624cbb8fdb
Lucas Fayolle <lfayolle1@gmail.com>
2016-04-24 17:52:27 +0200

    minor style fix work template

--- app/site/templates/work.php
+++ app/site/templates/work.php
@@ -3,9 +3,11 @@
 
   <main class="main work" role="main">
       <?php foreach($page->children()->visible() as $subpage): ?>
-      <section class="">
-        <h2><?php echo $subpage->title() ?></h2>
-        <a href="<?php echo $subpage->url() ?>">Launch gallery</a>
+      <section>
+        <div class="vertical-center">
+          <h2><?php echo $subpage->title() ?></h2>
+          <a href="<?php echo $subpage->url() ?>">Launch gallery</a>
+        </div>
       </section>
       <?php endforeach; ?>
   </main>
